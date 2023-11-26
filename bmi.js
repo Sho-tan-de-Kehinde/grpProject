a704e02fb6ac28b99165791bbffcb37eb91c392b
Shotande Kehinde <hussienk2011@gmail.com>
2023-11-26 21:41:58 +0100

    Update bmi.js

--- bmi.js
+++ bmi.js
@@ -9,7 +9,7 @@ calculateButton.addEventListener("click", ()=>{
     
     height = parseFloat(heightInput.value);
     weight = parseFloat(weightInput.value);
-    BMI = (parseFloat(weight/(height**2)).toFixed()); 
+    BMI = (parseFloat(weight/(height**2)).toFixed(2)); 
     result.innerText = BMI;
 
     if(BMI < 18.5){
@@ -17,8 +17,12 @@ calculateButton.addEventListener("click", ()=>{
     }else if((BMI > 18.5) && (BMI <= 24.9)){
         statement.innerText = "Your BMI falls within the normal or healthy weight range";
     }else if((BMI >= 25) && (BMI <= 29.9 )){
-        statement.innerText = "Your BMI falls within the overweight range";
+        statement.innerText = "Your BMI falls within the overweight range";  
     }else{
-        statement.innerText = "Your BMI falls within the obese range";
-    }
+        if(BMI > 29.9){
+            statement.innerText = "Your BMI falls within the obese range";    
+        }else if(BMI.length === ""){
+            statement.innerText = "NaN";
+        }
+    }  
 });
